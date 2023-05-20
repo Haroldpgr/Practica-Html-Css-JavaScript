398fdd517c0cb796661e9c55d3ed1bf172a03217
[Haroldpgr] <[haroladresamezquitanitola@gma.com]>
2023-05-19 20:32:51 -0500

    Practivas

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function mostrarDatos(){
+    fname = document.getElementById("fname").value
+     lname= document.getElementById("lname").value
+     document.getElementById("demo").innerHTML =  fname +  "   "  + lname
+
+ }
